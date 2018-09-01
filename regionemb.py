67c7e5e4a3fb7b836f346d84f53449fbad89cb3e
zhaozhengChen <35227544+zhaozhengChen@users.noreply.github.com>
2018-09-01 16:55:57 +0800

    Update regionemb.py
    optimize the running speed，twice faster than the original code

--- regionemb.py
+++ regionemb.py
@@ -18,6 +18,7 @@ list_vocab_size = [124273,394385,356312,42783,361926,227863]
 list_task = ['yelp_full','amazon_polarity','amazon_full','ag','yahoo','dbpedia']
 emb_size = 128
 region_size = 7
+region_radius = region_size/2
 batch_size = 16
 max_epoch = 20
 learning_rate = 0.0001
@@ -25,8 +26,8 @@ learning_rate = 0.0001
 #####################
 base_path = 'data/'
 print_step = 200
-ctx = mx.gpu(3)
-index = 1 # which task
+ctx = mx.gpu(2)
+index = 3 # which task
 n_classes = list_n_classes[index]
 vocab_size = list_vocab_size[index]
 max_sequence_length = list_max_sequence_length[index]
@@ -40,19 +41,13 @@ class Net(nn.HybridBlock):
             self.embedding_region = nn.Embedding(vocab_size,emb_size)
             self.max_pool = nn.GlobalMaxPool1D()
             self.dense = nn.Dense(n_classes)
-    def hybrid_forward(self, F, seq):
-        region_radius = region_size/2
-        aligned_seq = map(lambda i: F.slice(seq, begin=[None, i-region_radius], end=[None, i-region_radius+region_size]).asnumpy(), \
-                    range(region_radius, seq.shape[1] - region_radius))
+    def hybrid_forward(self, F,aligned_seq,trimed_seq,mask):
         region_aligned_seq = aligned_seq.transpose((1, 0, 2))
         region_aligned_emb = self.embedding_region(region_aligned_seq).reshape((batch_size,-1,region_size,emb_size))
-        trimed_seq = seq[:, region_radius: seq.shape[1] - region_radius]
         context_unit = self.embedding(trimed_seq).reshape((batch_size,-1,region_size,emb_size))
         projected_emb = region_aligned_emb * context_unit
         feature = self.max_pool(projected_emb.transpose((0,1,3,2)).reshape((batch_size,-1,region_size))).reshape((batch_size,-1,emb_size))
-        trimed_seq = seq[:,region_radius:seq.shape[1]-region_radius]
-        mask = F.greater(trimed_seq,0).reshape((batch_size,-1,1))
-        feature = mask*feature
+        feature = feature*mask
         res = F.sum(feature, axis=1).reshape((batch_size,emb_size))
         res = self.dense(res)
         return res
@@ -112,9 +107,22 @@ def load_data(path):
     test_path = base_path+task_path+'test.csv.id'
     labels_test, sequence_test = read_data(test_path, indexes, lengths)
     return list(zip(sequence_test, labels_test)),list(zip(sequence_train, labels_train))
-def evaluate(data):
-    acc_test = mx.metric.Accuracy()
+def accuracy(ouput,label,batch_size):
+    out = nd.argmax(output,axis=1)
+    res = nd.sum(nd.equal(out.reshape((-1,1)),label))/batch_size
+    return res
+def batch_process(seq,ctx):
+    batch_sequence = nd.array(seq,ctx)
+    aligned_seq = nd.zeros((max_sequence_length - 2*region_radius,batch_size,region_size),ctx)
+    for i in range(region_radius, max_sequence_length - region_radius):
+        aligned_seq[i-region_radius] = nd.slice(batch_sequence, begin=[None, i-region_radius], end=[None, i-region_radius+region_size])
+    batch_sequence = nd.array(batch_sequence,ctx)
+    trimed_seq = batch_sequence[:, region_radius: max_sequence_length - region_radius]
+    mask = nd.broadcast_axes(nd.greater(trimed_seq,0).reshape((batch_size,-1,1)),axis=2,size=128)
+    return aligned_seq,nd.array(trimed_seq,ctx),mask,tot
+def evaluate(data,batch_size):
     test_loss = 0.0
+    acc_test = 0.0
     cnt = 0
     for epoch_percent, batch_slots in batch_iter(data,batch_size,shuffle=False):
         batch_sequence, batch_label = zip(*batch_slots)
@@ -122,38 +130,37 @@ def evaluate(data):
         batch_label = nd.array(batch_label,ctx)
         output = net(batch_sequence)
         loss = SCE(output,batch_label)
-        acc_test.update(preds=[output],labels=[batch_label])
-        test_loss += nd.mean(loss).asscalar()
+        acc_test += accuracy(output,batch_label,batch_size)
+        test_loss += nd.mean(loss)
         cnt = cnt+1
-    return acc_test.get()[1],test_loss/cnt
+    return acc_test.asscalar(),test_loss.asscalar()/cnt
 net = Net()
 SCE = mx.gluon.loss.SoftmaxCrossEntropyLoss()
 net.initialize(init.Xavier(), ctx=ctx)
 trainer = gluon.Trainer(net.collect_params(),'adam', {'learning_rate': learning_rate})
 data_test,data_train = load_data(task_path)
 best_acc,global_step,train_loss,train_acc = 0,0,0,0
+net.hybridize()
 ctime = time.time()
 print(ctx,list_task[index])
 for epoch in range(max_epoch):
     for epoch_percent, batch_slots in batch_iter(data_train,batch_size,shuffle=True):
-        acc = mx.metric.Accuracy()
         batch_sequence, batch_label = zip(*batch_slots)
         global_step = global_step + 1
-        batch_sequence = nd.array(batch_sequence,ctx)
         batch_label = nd.array(batch_label,ctx)
+        aligned_seq,trimed_seq,mask,t = batch_process(batch_sequence,ctx)
         with autograd.record():
-            output = net(batch_sequence)
+            output = net(aligned_seq,trimed_seq,mask)
             loss = SCE(output,batch_label)
         loss.backward()
         trainer.step(batch_size)
-        acc.update(preds=[output],labels=[batch_label])
-        train_acc += acc.get()[1]
-        train_loss += nd.mean(loss).asscalar()
+        train_acc += accuracy(output,batch_label,batch_size)
+        train_loss += nd.mean(loss)
         if global_step%print_step==0:
-            print('%.4f %%'%epoch_percent,'train_loss:',train_loss/print_step,' train_acc:',train_acc/print_step,'time:',time.time()-ctime)
-            ctime = time.time()
+            print('%.4f %%'%epoch_percent,'train_loss:',train_loss.asscalar()/print_step,' train_acc:',train_acc.asscalar()/print_step,'time:',time.time()-ctime)
             train_loss,train_acc = 0,0
-    test_acc,test_loss = evaluate(data_test)
+            ctime = time.time()
+    test_acc,test_loss = evaluate(data_test，batch_size)
     if test_acc>best_acc:
         net.save_parameters('params/regionemb_'+list_task[index])
     print('epoch %d done'%(epoch+1),'acc = %.4f,loss = %.4f'%(test_acc,test_loss))    
